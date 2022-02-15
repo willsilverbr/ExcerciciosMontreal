0a081ea1cda9726ee5cb249d44a858f2a7440c01
William Barbosa <will.silver@hotmail.com>
2022-02-15 14:06:36 -0300

    Add project files.

---
+++ EstruturaRepeticao/Matematica.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaRepeticao
+{
+    class Matematica
+    {
+        public static  int Exponenciacao(int Base, int Exponente)
+        {
+            int resultado = Base;
+            if (Exponente == 1)
+            {
+                resultado = Base;
+            }
+            else
+
+            
+            for (int i = 1; i <Exponente; i ++ )
+            {
+                resultado = resultado * Base;  
+              
+            }
+            return resultado;
+        }
+    }
+}
---
+++ EstruturaRepeticao/NumeroExpoenteMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstruturaRepeticao
+{
+    class NumeroExpoenteMain
+    {
+        static void Main(string[] args)
+        {
+            /*
+             fazer um programa que precisa fazer um calculo com um metodo estatico chamado 
+            exponenciação 
+            
+             */
+            int b, e; 
+            Console.WriteLine("Digite o Valor da Base: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o Valor do Expoente: ");
+            e = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = Matematica.Exponenciacao(b, e);
+            Console.WriteLine(b+ " Elevado a " + e + " é : " + resultado);
+        }
+    }
+}
